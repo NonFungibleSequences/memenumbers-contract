0733a5177b504d6ebe4842d0f517da214fb6e2f8
Andrey Petrov <andrey.petrov@shazow.net>
2021-09-19 10:54:24 -0400

    src/test: Some skeleton tests

--- src/test/Memeonacci.t.sol
+++ src/test/Memeonacci.t.sol
@@ -6,4 +6,16 @@ import { Errors } from "../Memeonacci.sol";
 
 contract Meme is MemeonacciTest {
     // TODO: ...
+
+    function testMint() public {
+        try alice.mint(1) { fail(); } catch Error(string memory error) {
+            assertEq(error, Errors.NotForSale);
+        }
+
+        uint256[] memory forSale = meme.getForSale();
+        alice.mint(forSale[0]);
+
+        assertEq(meme.ownerOf(forSale[0]), address(alice));
+    }
 }
+
--- src/test/utils/MemeonacciTest.sol
+++ src/test/utils/MemeonacciTest.sol
@@ -5,11 +5,32 @@ import "ds-test/test.sol";
 import "../../Memeonacci.sol";
 import "./Hevm.sol";
 
+contract User {
+    Memeonacci internal meme;
+
+    constructor(address _meme) {
+        meme = Memeonacci(_meme);
+    }
+
+    function mint(uint256 num) public {
+        meme.mint(address(this), num);
+    }
+}
+
 contract MemeonacciTest is DSTest {
     Hevm internal constant hevm = Hevm(HEVM_ADDRESS);
 
-    // TODO: ...
+    // contracts
+    Memeonacci internal meme;
+
+    // users
+    User internal owner;
+    User internal alice;
+
     function setUp() public virtual {
-        // ...
+        meme = new Memeonacci();
+        owner = new User(address(meme));
+        alice = new User(address(meme));
+        meme.transferOwnership(address(owner));
     }
 }
