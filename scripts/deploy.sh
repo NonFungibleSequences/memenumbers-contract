0398f9c901431a65376708e6584d97752bb8798d
Andrey Petrov <andrey.petrov@shazow.net>
2021-10-09 16:23:46 -0400

    scripts/deploy.sh: Improve deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,19 @@
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
+#export DAPP_VERIFY_CONTRACT=1 # FIXME: This breaks :( Can't figure out how to pass it in properly
+
 # Deploy.
-RendererAddr=$(deploy MemeNumbersRenderer)
-ContractAddr=$(deploy MemeNumbers $RendererAddr)
-log "MemeNumbers deployed at:" $ContractAddr
+if [[ ! "$RendererAddr" ]]; then
+  RendererAddr=$(deploy MemeNumbersRenderer)
+  log "MemeNumbersRenderer deployed at:" $RendererAddr
+else
+  log "MemeNumbersRenderer already deployed, skipping: $RendererAddr"
+fi
+
+if [[ ! "$ContractAddr" ]]; then
+  ContractAddr=$(deploy MemeNumbers $RendererAddr)
+  log "MemeNumbers deployed at:" $ContractAddr
+else
+  log "MemeNumbers already deployed, skipping: $ContractAddr"
+fi
